4184bb345c05800a8fca9b9ac10da631832fb002
inock <millinya@naver.com>
2016-03-17 01:17:49 +0900

    - xcdatamodeld 정상연결

--- AccountBook/AppDelegate.swift
+++ AccountBook/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
-        //print(CoreDataManager.sharedManager.managedObjectContext)
+        print(CoreDataManager.sharedManager.managedObjectContext)
         
         return true
     }
